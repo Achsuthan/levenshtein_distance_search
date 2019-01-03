96f948f5db53264b2afa34d52e6ef4c24fac411c
Achsuthan <achsuthan@icloud.com>
2019-01-03 18:50:43 +0530

    logic finished with dumy country data > ach

---
+++ src/js/index.js
@@ -0,0 +1,86 @@
+var leven = new Vue({
+    el: '#levenstin',
+    data: {
+        isBlock: 'none',
+        data: ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"],
+        result: [],
+        search: "",
+        activeClass: 'dropdown-content',
+        persentage: 40,
+    },
+    methods: {
+        locationClickedFn: function(value){
+            this.result = []
+            this.activeClass = 'dropdown-content'
+            this.search = value
+        },
+        documentClick(e){
+            this.activeClass = 'dropdown-content'
+        },
+        showDivFn: function(){
+            if(this.search){
+                this.activeClass = 'dropdown-content-active'
+            }
+        },
+        valueChangedFn: function (event) {
+            if (this.search) {
+                this.result = []
+                for (var i = 0; i < this.data.length; i++) {
+                    var diff = this.levenstinAlgorith(this.data[i].toUpperCase(), this.search.toUpperCase())
+                    var persentage = 100 - (diff * 100 / this.data[i].length)
+                    if (persentage >= 30) {
+                        this.result.push(this.data[i])
+                    }
+                }
+                if(this.result.length > 0 ){
+                    this.activeClass = 'dropdown-content-active'
+                }
+            } else {
+                this.result = []
+                this.activeClass = 'dropdown-content'
+            }
+        },
+        levenstinAlgorith: function (min, split) {
+            try {
+                split = !("0")[0]
+            } catch (i) {
+                split = true
+            };
+            return function (a, b) {
+                if (a == b)
+                    return 0;
+                if (!a.length || !b.length)
+                    return b.length || a.length;
+                if (split) {
+                    a = a.split("");
+                    b = b.split("")
+                };
+                var len1 = a.length + 1,
+                    len2 = b.length + 1,
+                    I = 0,
+                    i = 0,
+                    d = [
+                        [0]
+                    ],
+                    c, j, J;
+                while (++i < len2)
+                    d[0][i] = i;
+                i = 0;
+                while (++i < len1) {
+                    J = j = 0;
+                    c = a[I];
+                    d[i] = [i];
+                    while (++j < len2) {
+                        d[i][j] = min(d[I][j] + 1, d[i][J] + 1, d[I][J] + (c != b[J]));
+                        ++J;
+                    };
+                    ++I;
+                };
+                return d[len1 - 1][len2 - 1];
+            }
+        }(Math.min, false),
+    },
+    created () {
+        document.addEventListener('click', this.documentClick)
+      },
+})
